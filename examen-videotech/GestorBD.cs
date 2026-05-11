d6568000722de6b504d9d29274681a80c8edf9df
Guillermo <guillermosapenaa@gmail.com>
2026-05-11 09:40:34 +0200

    Ejercicio 3: conexión y consultas MySQL

---
+++ examen-videotech/GestorBD.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+class GestorBD
+{
+	private MySqlConnection conexion;
+
+	// 3.1 - Constructor con cadena de conexión
+	public GestorBD()
+	{
+		MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+		builder.Server = "localhost";
+		builder.UserID = "root";
+		builder.Password = "";
+		builder.Database = "videotech";
+
+		conexion = new MySqlConnection(builder.ConnectionString);
+	}
+
+	// 3.2 Insertar
+	public void Insertar(Pelicula l)
+	{
+		conexion.Open();
+
+		MySqlCommand cmd = new MySqlCommand("INSERT INTO peliculas (titulo, director, anyo, disponible) VALUES (@titulo, @autor, @anyo, @disponible)", conexion);
+		cmd.Parameters.AddWithValue("@titulo", l.getTitulo());
+		cmd.Parameters.AddWithValue("@director", l.getDirector());
+		cmd.Parameters.AddWithValue("@anyo", l.getAnyo());
+		cmd.Parameters.AddWithValue("@disponible", l.isDisponible());
+
+		cmd.ExecuteNonQuery();
+
+		conexion.Close();
+	}
+	// 3.3 - Leer todos
+	public List<Pelicula> ObtenerTodos()
+	{
+		List<Pelicula> lista = new List<Pelicula>();
+
+		conexion.Open();
+
+		MySqlCommand cmd = new MySqlCommand("SELECT * FROM libros", conexion);
+		MySqlDataReader dr = cmd.ExecuteReader();
+
+		while (dr.Read())
+		{
+			Pelicula l = new Pelicula(
+				dr["titulo"].ToString(),
+				dr["director"].ToString(),
+				int.Parse(dr["anyo"].ToString()),
+				bool.Parse(dr["disponible"].ToString())
+			);
+			lista.Add(l);
+		}
+
+		dr.Close();
+		conexion.Close();
+
+		return lista;
+	}
+
+}
+
--- examen-videotech/Program.cs
+++ examen-videotech/Program.cs
@@ -1,19 +1,21 @@
 ﻿using System.IO;
 using System.Collections.Generic;
 
-
+//Apartado 2.1 - Trabajar con la lista
+//Apartado 2.1 A)
 List<Pelicula> peliculas = new List<Pelicula>();
 
 peliculas.Add(new Pelicula ("Titanic", "James Cameron", 1998, true));
 peliculas.Add(new Pelicula("El club de la lucha", "David Fincher", 1999, false));
 peliculas.Add(new Pelicula("Sinner", "Cristopher Nolan", 2024, false));
 
+//Apartado 2.1 B)
 Console.WriteLine(" Todas las películas ");
 foreach (Pelicula l in peliculas)
 {
 	Console.WriteLine(l.ToString());
 }
-
+//Apartado 2.1 C)
 Console.WriteLine("Películas de Nolan");
 foreach (Pelicula l in peliculas)
 {
@@ -22,8 +24,12 @@ foreach (Pelicula l in peliculas)
 		Console.WriteLine(l.ToString());
 	}
 }
+
+// Apartado 2.2 - Fecha de registro
 Console.WriteLine("Fecha de registro");
 Console.WriteLine(DateTime.Now.ToShortDateString());
+
+// Apartado 2.3 - Guardar en fichero
 
 StreamWriter writer = File.CreateText("Peliculas.txt");
 
